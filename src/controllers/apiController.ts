9d6698cdeb9b98c533227c3fbf4fa483c07c0004
Maike Sala <maikesala49@gmail.com>
2023-12-19 18:59:03 -0300

    Conexão com bando de dados feita

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { User } from '../models/User';
+
+export const pong = (req: Request, res: Response) => {
+    res.json({ping: true});
+}
+
+export const register = async (req: Request, res: Response) => {
+    if(req.body.email && req.body.password) {
+        let { email, password } = req.body;
+
+        let hasUser = await User.findOne({where: { email }});
+        if(!hasUser) {
+            let newUser = await User.create({ email, password });
+
+            res.status(201);
+            res.json({ id: newUser.id });
+        } else {
+            res.json({ error: 'E-mail já existe.' });
+        }
+    }
+
+    res.json({ error: 'E-mail e/ou senha não enviados.' });
+}
+
+export const login = async (req: Request, res: Response) => {
+    let { email, password, name, phone, adress, prefComunicacao, infoPagamento, datanascimento} = req.body;
+
+    if(email && password && name && phone && adress) {
+
+    }
+
+    res.json({ status: false });
+}
